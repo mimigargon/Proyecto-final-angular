5c26421df3c1e1acb006043fea516b5941ae8783
Jandrinupgrade <alejandro.suarez@bootcamp-upgrade.com>
2022-06-25 00:28:54 +0200

    FEAT: add library interface

---
+++ src/app/models/library.interface.ts
@@ -0,0 +1,13 @@
+export interface LibraryInterface {
+    id: number;
+    title: string;
+    autho: string;
+    cover: string;
+    genre: string;
+    publisher: string;
+    reservation: boolean;
+}
+
+    
+      
+    
