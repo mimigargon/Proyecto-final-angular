a7732f53d4f7e493a27392173f661ab84a70ceaa
mimigargon <mireiag77@gmail.com>
2022-06-25 13:12:35 +0200

    FEAT: Models created

--- src/app/models/library.interface.ts
+++ src/app/models/library.interface.ts
@@ -1,13 +1,51 @@
-export interface LibraryInterface {
+export interface UpdatesInterface {
     id: number;
     title: string;
-    autho: string;
+    author: string;
     cover: string;
     genre: string;
     publisher: string;
     reservation: boolean;
 }
 
+export interface UserInterface {
+    id: number,
+    username: string,
+    password: string,
+    books: [], 
+    member: string, 
+    reservations: string
+}
+
+export interface HomeInterface {
+title: string,
+text: string,
+image: ImageInterface;
+links: LinkInterface[];
+}
+
+export interface ImageInterface {
+    src: string,
+    alt: string, 
+}
+
+export interface BooksGalleryInterface {
+    title: string;
+    gallery: ImageInterface[];
+    text: string,
+    reservation: string,
+}
+
+export interface LinkInterface {
+    img: ImageInterface,
+    text: string, 
+    link: string, 
+}
+
+export interface FooterInterface {
+    logo: ImageInterface,
+    links: LinkInterface[],
+}
     
       
     
